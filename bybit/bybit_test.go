8992647d050c43329984915ec4d0306fc4a5522e
Cenk Arıöz <20242069+cnkarz@users.noreply.github.com>
2022-07-14 08:41:06 +0200

    comment out test (#35)

--- bybit/bybit_test.go
+++ bybit/bybit_test.go
@@ -1,7 +1,6 @@
 package bybit
 
 import (
-	"fmt"
 	"testing"
 	// "time"
 )
@@ -28,38 +27,40 @@ func TestSetDefaults(t *testing.T) {
 	}
 }
 
-func TestSendHTTPRequest(t *testing.T) {
-	var result = struct {
-		ResultWrapper
-		Result []Instrument `json:"result"`
-	}{}
+// Commented this test out as it is bad practice to make external HTTP calls
 
-	tests := []struct {
-		name        string
-		APIEndpoint string
-		expectedErr error
-	}{
-		{"valid endpoint",
-			"/v2/public/tickers",
-			nil,
-		},
-		{"invalid endpoint",
-			"aweiawjfasf",
-			fmt.Errorf("Get \"https://api.bybit.comaweiawjfasf\": dial tcp: lookup api.bybit.comaweiawjfasf: no such host"),
-		},
-	}
+// func TestSendHTTPRequest(t *testing.T) {
+// 	var result = struct {
+// 		ResultWrapper
+// 		Result []Instrument `json:"result"`
+// 	}{}
 
-	// 1. Test runs and fetches requests as expected
-	// 2. Test fails as expected
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			err := mockBybit.SendHTTPRequest(tt.APIEndpoint, &GenericRequestParams{}, &result)
-			if err != nil && err.Error() != tt.expectedErr.Error() {
-				t.Errorf("expected error got = %v, want = %v", err, tt.expectedErr)
-			}
-		})
-	}
-}
+// 	tests := []struct {
+// 		name        string
+// 		APIEndpoint string
+// 		expectedErr error
+// 	}{
+// 		{"valid endpoint",
+// 			"/v2/public/tickers",
+// 			nil,
+// 		},
+// 		{"invalid endpoint",
+// 			"aweiawjfasf",
+// 			fmt.Errorf("Get \"https://api.bybit.comaweiawjfasf\": dial tcp: lookup api.bybit.comaweiawjfasf: no such host"),
+// 		},
+// 	}
+
+// 	// 1. Test runs and fetches requests as expected
+// 	// 2. Test fails as expected
+// 	for _, tt := range tests {
+// 		t.Run(tt.name, func(t *testing.T) {
+// 			err := mockBybit.SendHTTPRequest(tt.APIEndpoint, &GenericRequestParams{}, &result)
+// 			if err != nil && err.Error() != tt.expectedErr.Error() {
+// 				t.Errorf("expected error got = %v, want = %v", err, tt.expectedErr)
+// 			}
+// 		})
+// 	}
+// }
 
 func TestGetInstruments(t *testing.T) {
 	ts, instruments, err := mockBybit.GetInstruments(&GenericRequestParams{})
