f99cfd07d9169c6401bb53d73014e7652db11aa0
Gunnar Sundberg <gunnarsundberg@pm.me>
2022-10-12 22:07:43 -0400

    error handling, config, remove extra goroutines

--- near/subscription.go
+++ near/subscription.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"embed"
 	"fmt"
+	"math/rand"
 	"net/url"
 	"os"
 	"os/exec"
@@ -15,12 +16,6 @@ import (
 	"github.com/rs/zerolog/log"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/reflect/protoreflect"
-)
-
-const (
-	channelSize  = 1000
-	port         = "3030"
-	backfillPort = "3031"
 )
 
 //go:embed target/release/nakji_near_client
@@ -37,6 +32,7 @@ type Subscription struct {
 	host                 string
 	port                 string
 	backfillPort         string
+	maxRetries           int
 	fromBlock            uint64
 	numBlocks            uint64
 	events               []string
@@ -50,7 +46,7 @@ type Subscription struct {
 	errChan              chan error
 }
 
-func NewSubscription(ctx context.Context, host string, msgTypes []proto.Message, events []string, fromBlock uint64, numBlocks uint64) (*Subscription, error) {
+func NewSubscription(ctx context.Context, config *Config, events []string) (*Subscription, error) {
 	bin, err := f.ReadFile("target/release/nakji_near_client")
 	if err != nil {
 		log.Fatal().Err(err).Msg("failed read embedded Nakji Near Client binary")
@@ -62,20 +58,21 @@ func NewSubscription(ctx context.Context, host string, msgTypes []proto.Message,
 	}
 
 	sub := &Subscription{
-		host:                 host,
-		port:                 port,
-		backfillPort:         backfillPort,
+		host:                 config.Host,
+		port:                 config.Port,
+		backfillPort:         config.BackfillPort,
+		maxRetries:           config.MaxRetries,
 		events:               events,
-		msgTypes:             msgTypes,
-		fromBlock:            fromBlock,
-		numBlocks:            numBlocks,
+		msgTypes:             config.MsgTypes,
+		fromBlock:            config.FromBlock,
+		numBlocks:            config.NumBlocks,
 		done:                 make(chan struct{}),
 		ctx:                  ctx,
-		blockChan:            make(chan proto.Message, channelSize),
-		transactionChan:      make(chan proto.Message, channelSize),
-		receiptChan:          make(chan proto.Message, channelSize),
-		executionOutcomeChan: make(chan proto.Message, channelSize),
-		errChan:              make(chan error, channelSize),
+		blockChan:            make(chan proto.Message, config.ChannelSize),
+		transactionChan:      make(chan proto.Message, config.ChannelSize),
+		receiptChan:          make(chan proto.Message, config.ChannelSize),
+		executionOutcomeChan: make(chan proto.Message, config.ChannelSize),
+		errChan:              make(chan error, config.ChannelSize),
 	}
 	go func() {
 		interrupt := make(chan os.Signal, 1)
@@ -123,26 +120,27 @@ func (s *Subscription) Err() <-chan error {
 
 func (s *Subscription) subscribe() {
 	// Start Lake Stream from current block
-	go s.initLakeStream(s.port, 0, 0)
+	go s.startLakeStream(s.port, 0, 0)
+	s.initWsStreams(false)
 	// Backfill if needed
 	if s.fromBlock > 0 || s.numBlocks > 0 {
-		go s.backfill()
+		s.backfill()
 	}
 }
 
 func (s *Subscription) backfill() {
 	log.Info().Msg("start backfilling")
 	if s.fromBlock > 0 {
-		//s.getEvents(s.fromBlock, latestBlock)
-		s.initLakeStream(s.backfillPort, s.fromBlock, 0)
+		go s.startLakeStream(s.backfillPort, s.fromBlock, 0)
+		s.initWsStreams(true)
 	} else if s.numBlocks > 0 {
-		//s.getEvents(latestBlock-s.numBlocks, latestBlock)
-		s.initLakeStream(s.backfillPort, 0, s.numBlocks)
+		go s.startLakeStream(s.backfillPort, 0, s.numBlocks)
+		s.initWsStreams(true)
 	}
 }
 
-func (s *Subscription) initLakeStream(port string, fromBlock uint64, numBlocks uint64) {
-	isBackfill := false
+func (s *Subscription) startLakeStream(port string, fromBlock uint64, numBlocks uint64) {
+	ctx := context.Background()
 
 	// Setup command for Lake stream
 	cmd := []string{
@@ -155,16 +153,14 @@ func (s *Subscription) initLakeStream(port string, fromBlock uint64, numBlocks u
 	if fromBlock > 0 {
 		cmd = append(cmd, "from-block")
 		cmd = append(cmd, fmt.Sprintf("%d", fromBlock))
-		isBackfill = true
 	} else if numBlocks > 0 {
 		cmd = append(cmd, "num-blocks")
 		cmd = append(cmd, fmt.Sprintf("%d", numBlocks))
-		isBackfill = true
 	} else {
 		cmd = append(cmd, "from-latest")
 	}
 
-	execCmd := exec.CommandContext(s.ctx, cmd[0], cmd[1:]...)
+	execCmd := exec.CommandContext(ctx, cmd[0], cmd[1:]...)
 	execCmd.Stdout = os.Stdout
 	execCmd.Stderr = os.Stderr
 
@@ -176,45 +172,81 @@ func (s *Subscription) initLakeStream(port string, fromBlock uint64, numBlocks u
 		}
 	}()
 
-	// Allow time for ws server to start
-	time.Sleep(5 * time.Second)
-
-	// Connect to ws server to receive data from Lake stream
-	for endpoint, msgType := range endpointMap {
-		// Check if msgType is in sub's msgTypes
-		for _, subType := range s.msgTypes {
-			if reflect.TypeOf(msgType) == reflect.TypeOf(subType) {
-				go s.wsStream(port, endpoint, msgType, isBackfill)
-			}
+	// Ensure that the binaary does not continue running after shutdown
+	for {
+		select {
+		case <-s.done:
+			ctx.Done()
+			return
+		default:
+			continue
 		}
 	}
 
 }
 
-func (s *Subscription) wsStream(port string, endpoint string, msgType proto.Message, isBackfill bool) {
+func (s *Subscription) initWsStreams(isBackfill bool) {
+	// Connect to ws server to receive data from Lake stream
+	for endpoint, msgType := range endpointMap {
+		// Check if msgType is in sub's msgTypes
+		for _, subType := range s.msgTypes {
+			if reflect.TypeOf(msgType) == reflect.TypeOf(subType) {
+				go s.startWsStream(endpoint, msgType, isBackfill)
+			}
+		}
+	}
+}
+
+func (s *Subscription) startWsStream(endpoint string, msgType proto.Message, isBackfill bool) {
+	// Get port from config
+	var port string
+	if isBackfill {
+		port = s.backfillPort
+	} else {
+		port = s.port
+	}
+
+	// Construct ws url
 	host := fmt.Sprintf("%s%s%s", s.host, ":", port)
 	u := url.URL{Scheme: "ws", Host: host, Path: endpoint}
 	log.Info().Str("host", host).Str("endpoint", endpoint).Msg("connecting to ws server")
 
 	// Output chan determined by msgType
-	var ch chan protoreflect.ProtoMessage
-	switch reflect.TypeOf(msgType) {
-	case reflect.TypeOf(&Block{}):
-		ch = s.blockChan
-	case reflect.TypeOf(&Transaction{}):
-		ch = s.transactionChan
-	case reflect.TypeOf(&Receipt{}):
-		ch = s.receiptChan
-	case reflect.TypeOf(&ExecutionOutcome{}):
-		ch = s.executionOutcomeChan
+	ch := s.getMsgChan(msgType)
+	if ch == nil {
+		log.Error().Msg("could not determine output channel")
+		return
 	}
 
-	conn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
-	if err != nil {
-		log.Fatal().Err(err).Msg(fmt.Sprintf("%s Error connecting to websocket", err))
+	// Connect to ws server with retry
+	var conn *websocket.Conn
+	for retry := 0; ; retry++ {
+		tryConn, _, err := websocket.DefaultDialer.Dial(u.String(), nil)
+		if err != nil {
+			if retry < s.maxRetries {
+				// Do exponential backoff with 10% jitter
+				backoff := float64(int(1) << retry)
+				backoff += backoff * (0.1 * rand.Float64())
+				select {
+				case <-s.done:
+					return
+				case <-time.After(time.Second * time.Duration(backoff)):
+					continue
+				}
+			}
+			log.Fatal().Err(err).Msg(fmt.Sprintf("%s Unable to connect to NEAR WS Server", err))
+		}
+		conn = tryConn
+		break
 	}
+
 	defer conn.Close()
 
+	if isBackfill {
+		defer log.Info().Msg("stop backfilling")
+	}
+
+	// Read messages from connection
 	for {
 		select {
 		case <-s.done:
@@ -225,16 +257,33 @@ func (s *Subscription) wsStream(port string, endpoint string, msgType proto.Mess
 				log.Debug().Msg(fmt.Sprintf("%s Backfill ws connection closed", err))
 				return
 			} else if err != nil {
-				log.Fatal().Err(err).Msg(fmt.Sprintf("%s Error reading ws message", err))
+				s.errChan <- err
+				continue
 			}
 
 			msg := proto.Clone(msgType)
 
 			if err := proto.Unmarshal(wsMessage, msg); err != nil {
-				log.Fatal().Err(err).Msg(fmt.Sprintf("%s Error unmarshalling message: ", err))
+				s.errChan <- err
+				continue
 			}
 
 			ch <- msg
 		}
+	}
+}
+
+func (s *Subscription) getMsgChan(msgType proto.Message) chan protoreflect.ProtoMessage {
+	switch reflect.TypeOf(msgType) {
+	case reflect.TypeOf(&Block{}):
+		return s.blockChan
+	case reflect.TypeOf(&Transaction{}):
+		return s.transactionChan
+	case reflect.TypeOf(&Receipt{}):
+		return s.receiptChan
+	case reflect.TypeOf(&ExecutionOutcome{}):
+		return s.executionOutcomeChan
+	default:
+		return nil
 	}
 }
