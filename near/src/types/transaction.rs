a077caa1735918c09cdc071305bfaab49fd2f2ad
Gunnar Sundberg <gunnarsundberg@pm.me>
2022-10-24 14:31:48 -0400

    move actions logic to mod for reuse

--- near/src/types/transaction.rs
+++ near/src/types/transaction.rs
@@ -1,18 +1,15 @@
 use std::time::Duration;
 
 use crate::near_proto::access_key::{FullAccessPermission, FunctionCallPermission, Permission};
-use crate::near_proto::{
-    action::{self, AddKey, CreateAccount, DeleteAccount, DeleteKey, FunctionCall, Stake},
-    AccessKey, Action, Transaction,
-};
-use action::{DeployContract, Transfer};
+use crate::near_proto::{AccessKey, Transaction};
 use crossbeam_channel::Sender;
 use near_lake_framework::near_indexer_primitives::{
-    views::{AccessKeyPermissionView, AccessKeyView, ActionView},
+    views::{AccessKeyPermissionView, AccessKeyView},
     IndexerTransactionWithOutcome, StreamerMessage,
 };
 use prost_types::Timestamp;
 
+use super::action_from_view;
 use super::handle::ParseStreamMessage;
 
 impl ParseStreamMessage<Transaction> for Transaction {
@@ -23,9 +20,9 @@ impl ParseStreamMessage<Transaction> for Transaction {
             seconds: ts_duration.as_secs() as i64,
             nanos: ts_duration.subsec_nanos() as i32,
         };
-        for shard in &message.shards {
+        for shard in message.shards {
             if shard.chunk.is_some() {
-                let transactions = &shard.chunk.as_ref().unwrap().transactions;
+                let transactions = shard.chunk.unwrap().transactions;
                 for tx in transactions {
                     let mut tx_proto = Transaction::from(tx);
                     // Set transaction ts to block ts
@@ -40,85 +37,31 @@ impl ParseStreamMessage<Transaction> for Transaction {
     }
 }
 
-impl From<&IndexerTransactionWithOutcome> for Transaction {
+impl From<IndexerTransactionWithOutcome> for Transaction {
     fn from(
         IndexerTransactionWithOutcome {
             transaction,
             outcome: _,
-        }: &IndexerTransactionWithOutcome,
+        }: IndexerTransactionWithOutcome,
     ) -> Self {
-        let mut actions_proto = Vec::<Action>::new();
-
-        for action in &transaction.actions {
-            match action {
-                ActionView::CreateAccount => actions_proto.push(Action {
-                    action: Some(action::Action::CreateAccount(CreateAccount {})),
-                }),
-                ActionView::DeployContract { code } => actions_proto.push(Action {
-                    action: Some(action::Action::DeployContract(DeployContract {
-                        code: code.to_string(),
-                    })),
-                }),
-                ActionView::FunctionCall {
-                    method_name,
-                    args,
-                    gas,
-                    deposit,
-                } => actions_proto.push(Action {
-                    action: Some(action::Action::FunctionCall(FunctionCall {
-                        method_name: method_name.to_string(),
-                        args: args.to_string(),
-                        gas: *gas,
-                        deposit: deposit.to_ne_bytes().to_vec(),
-                    })),
-                }),
-                ActionView::Transfer { deposit } => actions_proto.push(Action {
-                    action: Some(action::Action::Transfer(Transfer {
-                        deposit: deposit.to_ne_bytes().to_vec(),
-                    })),
-                }),
-                ActionView::Stake { stake, public_key } => actions_proto.push(Action {
-                    action: Some(action::Action::Stake(Stake {
-                        stake: stake.to_ne_bytes().to_vec(),
-                        public_key: public_key.to_string(),
-                    })),
-                }),
-                ActionView::AddKey {
-                    public_key,
-                    access_key,
-                } => actions_proto.push(Action {
-                    action: Some(action::Action::AddKey(AddKey {
-                        public_key: public_key.to_string(),
-                        access_key: Some(AccessKey::from(access_key)),
-                    })),
-                }),
-                ActionView::DeleteKey { public_key } => actions_proto.push(Action {
-                    action: Some(action::Action::DeleteKey(DeleteKey {
-                        public_key: public_key.to_string(),
-                    })),
-                }),
-                ActionView::DeleteAccount { beneficiary_id } => actions_proto.push(Action {
-                    action: Some(action::Action::DeleteAccount(DeleteAccount {
-                        beneficiary_id: beneficiary_id.to_string(),
-                    })),
-                }),
-            }
-        }
-
         Self {
             signer_id: transaction.signer_id.to_string(),
             public_key: transaction.public_key.to_string(),
             nonce: transaction.nonce,
             receiver_id: transaction.receiver_id.to_string(),
             hash: transaction.hash.to_string(),
-            actions: actions_proto,
+            actions: transaction
+                .actions
+                .iter()
+                .map(|a| action_from_view(a.to_owned()))
+                .collect(),
             ts: None,
         }
     }
 }
 
-impl From<&AccessKeyView> for AccessKey {
-    fn from(AccessKeyView { nonce, permission }: &AccessKeyView) -> Self {
+impl From<AccessKeyView> for AccessKey {
+    fn from(AccessKeyView { nonce, permission }: AccessKeyView) -> Self {
         let permission_proto: Permission;
         match permission {
             AccessKeyPermissionView::FunctionCall {
@@ -137,7 +80,7 @@ impl From<&AccessKeyView> for AccessKey {
             }
         }
         Self {
-            nonce: *nonce,
+            nonce,
             permission: Some(permission_proto),
         }
     }
