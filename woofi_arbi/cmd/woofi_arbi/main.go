f5f4b0acf890e7af62b16836a3736a29997f6081
Hung Doan Phi <hugjobk@gmail.com>
2022-12-27 15:12:11 +0700

    Add woofi arbi (#159)
    Co-authored-by: Doan Phi Hung <davidhung@blep.ai>

---
+++ woofi_arbi/cmd/woofi_arbi/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"github.com/nakji-network/woofi-connectors/woofi"
+	"github.com/nakji-network/woofi-connectors/woofi/WooCrossChainRouterV1"
+	"github.com/nakji-network/woofi-connectors/woofi/WooPPV4"
+	"github.com/nakji-network/woofi-connectors/woofi/WooRouterV3"
+	"github.com/nakji-network/woofi-connectors/woofi/WooracleV2"
+
+	"github.com/spf13/pflag"
+	_ "go.uber.org/automaxprocs"
+)
+
+func main() {
+	fromBlock := pflag.Uint64P("from-block", "f", 0, "block number to start backfill from (optional)")
+	numBlocks := pflag.Uint64P("num-blocks", "b", 0, "number of blocks to backfill (optional)")
+
+	pflag.Parse()
+
+	conf := &woofi.Config{
+		NetworkName: "arbitrum",
+		FromBlock:   *fromBlock,
+		NumBlocks:   *numBlocks,
+	}
+
+	c := woofi.New(conf)
+	c.AddContract(WooPPV4.NewContract("0xeFF23B4bE1091b53205E35f3AfCD9C7182bf3062"))               // WooPPV2
+	c.AddContract(WooracleV2.NewContract("0x37a9dE70b6734dFCA54395D8061d9411D9910739"))            // WooracleV2
+	c.AddContract(WooRouterV3.NewContract("0x9aEd3A8896A85FE9a8CAc52C9B402D092B629a30"))           // WooRouterV2
+	c.AddContract(WooCrossChainRouterV1.NewContract("0x0972A0fa37984E7FF2aEFA53A0Bb10dCE535aa73")) // WooCrossChainRouterV1
+	c.Start()
+}
