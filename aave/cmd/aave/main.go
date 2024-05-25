6e9adc1f6026a88a7c5fa90147a3015641a26147
Sr. D <h@trylotus.xyz>
2024-05-25 18:36:34 +0700

    fix (aave): update connector lib and refactor

--- aave/cmd/aave/main.go
+++ aave/cmd/aave/main.go
@@ -1,71 +1,67 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"os"
+	"os/signal"
 	"time"
-
-	"github.com/trylotus/connector"
-	"github.com/trylotus/connector/config"
-	"github.com/trylotus/connectors/aave"
-	"github.com/trylotus/connectors/aave/lendingpool"
 
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/pflag"
+	connector "github.com/trylotus/connector/chain/ethereum"
+	"github.com/trylotus/connector/kafkautils"
+	"github.com/trylotus/connectors/aave"
+	"github.com/trylotus/connectors/aave/lendingpool"
 	_ "go.uber.org/automaxprocs"
 )
 
 func main() {
-	c, err := connector.NewConnector()
-	if err != nil {
-		log.Fatal().Err(err).Msg("failed to instantiate connector")
+	fromBlock := pflag.Uint64P("from-block", "f", 0, "block number to start backfill from (optional)")
+	numBlocks := pflag.Uint64P("num-blocks", "b", 0, "number of blocks to backfill (optional)")
+
+	pflag.Parse()
+
+	contracts := []connector.SmartContract{
+		lendingpool.NewContract(aave.LendingPoolContractAddr),
 	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		interrupt := make(chan os.Signal, 1)
+		signal.Notify(interrupt, os.Interrupt)
+
+		<-interrupt
+		log.Info().Msg("connector shutdown")
+		cancel()
+	}()
+
+	c := connector.NewConnector(ctx, "ethereum", contracts)
 
 	c.Config.SetDefault("aave.author", "lotus")
 	c.Config.SetDefault("aave.version", "0_0_0")
 	c.Config.SetDefault("blockTime", 15*time.Second)
 	c.Config.SetDefault("waitBlocks", 4)
 
-	pflag.Int64P("from-block", "f", 0, "block number to start backfill from (optional")
-	pflag.Int64P("num-blocks", "b", 0, "number of blocks to backfill (optional)")
-
-	pflag.Parse()
-	c.Config.BindPFlags(pflag.CommandLine)
-
-	if err := validateFlags(c.Config); err != nil {
-		log.Fatal().Err(err).Msg("input is not correct")
-	}
-
 	// Register topic and protobuf type mappings
-	c.RegisterProtos(aave.TopicTypes...)
+	c.RegisterProtos(kafkautils.MsgTypeFct, aave.TopicTypes...)
 
-	conf := &aave.Config{
-		ConnectorName: "aave",
-		NetworkName:   "ethereum",
-		FromBlock:     c.Config.GetUint64("from-block"),
-		NumBlocks:     c.Config.GetUint64("num-blocks"),
-		Namespace:     "aave",
+	if *fromBlock > 0 {
+		go func() {
+			msgs := c.Backfill(ctx, *fromBlock, *fromBlock+*numBlocks)
+			for msg := range msgs {
+				c.EventSink <- &kafkautils.Message{
+					MsgType:  kafkautils.MsgTypeBf,
+					ProtoMsg: msg,
+				}
+			}
+		}()
 	}
 
-	lendingPoolContract, err := lendingpool.NewContract(aave.LendingPoolContractAddr)
-	if err != nil {
-		log.Err(err).Msg("Cannot create lending pool contract")
+	msgs := c.Subscribe(ctx)
+	for msg := range msgs {
+		c.EventSink <- &kafkautils.Message{
+			MsgType:  kafkautils.MsgTypeFct,
+			ProtoMsg: msg,
+		}
 	}
-
-	m := aave.New(c, conf)
-	m.AddContract(lendingPoolContract)
-	m.Start()
-}
-
-func validateFlags(conf config.IConfig) error {
-	fromBlock := conf.GetInt64("from-block")
-	numBlocks := conf.GetInt64("num-blocks")
-
-	if fromBlock < 0 {
-		return fmt.Errorf("backfill input value cannot be negative. from-block: %d", fromBlock)
-	}
-
-	if numBlocks < 0 {
-		return fmt.Errorf("backfill input value cannot be negative. num-blocks: %d", numBlocks)
-	}
-	return nil
 }
--- aave/lendingpool/lendingpool.go
+++ aave/lendingpool/lendingpool.go
@@ -17,30 +17,28 @@ type SmartContract struct {
 	Abi  abi.ABI
 }
 
-func NewContract(addr string) (*SmartContract, error) {
+func NewContract(addr string) *SmartContract {
 	contractAbi, err := abi.JSON(strings.NewReader(LendingpoolABI))
 	if err != nil {
-		return nil, fmt.Errorf("error reading aave lending pool abi: %s", err)
+		log.Fatal().Err(err).Msg("error reading aave lending pool abi")
 	}
-	return &SmartContract{Addr: addr, Abi: contractAbi}, nil
+	return &SmartContract{Addr: addr, Abi: contractAbi}
 }
 
 func (c *SmartContract) Address() string {
 	return c.Addr
 }
 
-func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto.Message {
+func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) (proto.Message, error) {
 	ev, err := c.Abi.EventByID(vLog.Topics[0])
 	if err != nil {
-		log.Warn().Err(err).Msg("EventByID error, skipping")
-		return nil
+		return nil, fmt.Errorf("cannot find event by ID: %v", err)
 	}
 	switch ev.Name {
 	case "Borrow":
 		event := new(LendingpoolBorrow)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Borrow{
 			Ts:             ts,
@@ -54,12 +52,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			BorrowRate:     event.BorrowRate.Bytes(),
 			BorrowRateMode: event.BorrowRateMode.Bytes(),
 			Referral:       uint32(event.Referral),
-		}
+		}, nil
 	case "Deposit":
 		event := new(LendingpoolDeposit)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Deposit{
 			Ts:          ts,
@@ -71,12 +68,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			OnBehalfOf:  event.OnBehalfOf.Bytes(),
 			Amount:      event.Amount.Bytes(),
 			Referral:    uint32(event.Referral),
-		}
+		}, nil
 	case "FlashLoan":
 		event := new(LendingpoolFlashLoan)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &FlashLoan{
 			Ts:           ts,
@@ -89,12 +85,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			Amount:       event.Amount.Bytes(),
 			Premium:      event.Premium.Bytes(),
 			ReferralCode: uint32(event.ReferralCode),
-		}
+		}, nil
 	case "LiquidationCall":
 		event := new(LendingpoolLiquidationCall)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &LiquidationCall{
 			Ts:                         ts,
@@ -108,12 +103,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			LiquidatedCollateralAmount: event.LiquidatedCollateralAmount.Bytes(),
 			Liquidator:                 event.Liquidator.Bytes(),
 			ReceiveAToken:              event.ReceiveAToken,
-		}
+		}, nil
 	case "RebalanceStableBorrowRate":
 		event := new(LendingpoolRebalanceStableBorrowRate)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &RebalanceStableBorrowRate{
 			Ts:          ts,
@@ -122,12 +116,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			TxHash:      vLog.TxHash.Bytes(),
 			Reserve:     event.Reserve.Bytes(),
 			User:        event.User.Bytes(),
-		}
+		}, nil
 	case "Repay":
 		event := new(LendingpoolRepay)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Repay{
 			Ts:          ts,
@@ -138,12 +131,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			User:        event.User.Bytes(),
 			Repayer:     event.Repayer.Bytes(),
 			Amount:      event.Amount.Bytes(),
-		}
+		}, nil
 	case "ReserveDataUpdated":
 		event := new(LendingpoolReserveDataUpdated)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &ReserveDataUpdated{
 			Ts:                  ts,
@@ -155,12 +147,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			StableBorrowRate:    event.StableBorrowRate.Bytes(),
 			VariableBorrowIndex: event.VariableBorrowIndex.Bytes(),
 			VariableBorrowRate:  event.VariableBorrowRate.Bytes(),
-		}
+		}, nil
 	case "ReserveUsedAsCollateralDisabled":
 		event := new(LendingpoolReserveUsedAsCollateralDisabled)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &ReserveUsedAsCollateralDisabled{
 			Ts:          ts,
@@ -169,12 +160,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			TxHash:      vLog.TxHash.Bytes(),
 			Reserve:     event.Reserve.Bytes(),
 			User:        event.User.Bytes(),
-		}
+		}, nil
 	case "ReserveUsedAsCollateralEnabled":
 		event := new(LendingpoolReserveUsedAsCollateralEnabled)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &ReserveUsedAsCollateralEnabled{
 			Ts:          ts,
@@ -183,12 +173,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			TxHash:      vLog.TxHash.Bytes(),
 			Reserve:     event.Reserve.Bytes(),
 			User:        event.User.Bytes(),
-		}
+		}, nil
 	case "Swap":
 		event := new(LendingpoolSwap)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Swap{
 			Ts:          ts,
@@ -198,12 +187,11 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			Reserve:     event.Reserve.Bytes(),
 			User:        event.User.Bytes(),
 			RateMode:    event.RateMode.Bytes(),
-		}
+		}, nil
 	case "Withdraw":
 		event := new(LendingpoolWithdraw)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Withdraw{
 			Ts:          ts,
@@ -214,33 +202,30 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) proto
 			User:        event.User.Bytes(),
 			To:          event.To.Bytes(),
 			Amount:      event.Amount.Bytes(),
-		}
+		}, nil
 	case "Paused":
 		event := new(LendingpoolPaused)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Paused{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			LogIndex:    uint64(vLog.Index),
 			TxHash:      vLog.TxHash.Bytes(),
-		}
+		}, nil
 	case "Unpaused":
 		event := new(LendingpoolUnpaused)
 		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
-			log.Error().Err(err).Msgf("error unpacking event: %s", ev.Name)
-			return nil
+			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Unpaused{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			LogIndex:    uint64(vLog.Index),
 			TxHash:      vLog.TxHash.Bytes(),
-		}
+		}, nil
 	default:
-		log.Error().Msgf("invalid event: %s", ev.Name)
-		return nil
+		return nil, fmt.Errorf("invalid event: %s", ev.Name)
 	}
 }
