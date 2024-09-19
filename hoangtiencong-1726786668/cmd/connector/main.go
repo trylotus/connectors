b83efcbc2cce0cf178dd8b413ddd31b436229a91
TrylotusBot <TrylotusBot@trylotus.xyz>
2024-09-20 05:57:51 +0700

    [connector-bot] generated connector for Cyber Mainnet|hoangtiencong-1726786668

---
+++ hoangtiencong-1726786668/cmd/connector/main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/pflag"
+
+	"github.com/trylotus/go-connector"
+	"github.com/trylotus/go-connector/common"
+	"github.com/trylotus/go-connector/log"
+	"github.com/trylotus/go-connector/source/evm"
+
+    
+	"github.com/trylotus/connectors/hoangtiencong-1726786668/contracts/quest_object"
+)
+
+var ContractList = []evm.SmartContract{
+    
+        quest_object.NewContract("0x3D8C06e65ebf06A9d40F313a35353be06BD46038"),
+}
+
+func main() {
+	_ = godotenv.Load()
+
+	var (
+		subscribe     bool
+		subscribeFrom int64
+		backfill      common.List
+	)
+
+	pflag.VarP(&backfill, "backfill", "b", "comma separated list of block numbers to backfill (optional)")
+	pflag.BoolVarP(&subscribe, "subscribe", "s", true, "whether to subscribe to new blockchain events")
+	pflag.Int64VarP(&subscribeFrom, "subscribe-from", "f", 0, "block number to subscribe from (default latest block)")
+
+	pflag.Parse()
+
+	ctx, cancel := common.ContextWithSignal(context.Background(), os.Interrupt)
+	defer cancel()
+
+	source := evm.NewSource(ctx, os.Getenv("RPC_URL"), ContractList)
+
+	c := connector.NewConnector(source, connector.WithDefaultOptions())
+
+	go c.RegisterDescriptor(ctx,
+        
+            quest_object.File_quest_object_contract_proto,
+	)
+
+	if subscribe {
+		c.Subscribe(ctx, subscribeFrom)
+	}
+
+	if !backfill.IsEmpty() {
+		c.Backfill(ctx, backfill)
+	}
+
+	if err := c.Run(ctx); err != nil {
+		log.Error().Err(err).Msg("Connector shutdown")
+		return
+	}
+
+	log.Info().Msg("Connector shutdown")
+}
