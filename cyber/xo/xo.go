ae170ddd59397bd14929f95c6c5ba97761b4f852
Sr. D <h@trylotus.xyz>
2024-09-20 14:04:45 +0700

    feat(cyber): rename field index to log_index

--- cyber/xo/xo.go
+++ cyber/xo/xo.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/rs/zerolog/log"
 	"github.com/trylotus/go-connector/common"
+	"github.com/trylotus/go-connector/source/evm"
 	"google.golang.org/protobuf/proto"
 	timestamppb "google.golang.org/protobuf/types/known/timestamppb"
 )
@@ -17,10 +18,12 @@ type SmartContract struct {
 	Abi  abi.ABI
 }
 
+var _ evm.SmartContract = (*SmartContract)(nil)
+
 func NewContract(addr string) *SmartContract {
-	contractAbi, err := abi.JSON(strings.NewReader(XoABI))
+	contractAbi, err := abi.JSON(strings.NewReader(XoMetaData.ABI))
 	if err != nil {
-		log.Fatal().Err(err).Msg("error reading cyber XoABI")
+		log.Fatal().Err(err).Msg("error reading xo ABI")
 	}
 	return &SmartContract{Addr: addr, Abi: contractAbi}
 }
@@ -36,160 +39,160 @@ func (c *SmartContract) Message(vLog types.Log, ts *timestamppb.Timestamp) (prot
 	}
 	switch ev.Name {
 	case "PaidDM":
-		event := new(XoPaidDM)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoPaidDM
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &PaidDM{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			UserId:      event.UserId.Bytes(),
 			Denom:       event.Denom.Bytes(),
 			Amount:      event.Amount.String(),
 		}, nil
 	case "RoleAdminChanged":
-		event := new(XoRoleAdminChanged)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoRoleAdminChanged
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &RoleAdminChanged{
 			Ts:                ts,
 			BlockNumber:       vLog.BlockNumber,
 			TxHash:            vLog.TxHash.Bytes(),
-			Index:             uint64(vLog.Index),
+			LogIndex:          uint64(vLog.Index),
 			Role:              event.Role[:],
 			PreviousAdminRole: event.PreviousAdminRole[:],
 			NewAdminRole:      event.NewAdminRole[:],
 		}, nil
 	case "SBTUpdated":
-		event := new(XoSBTUpdated)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoSBTUpdated
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &SBTUpdated{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			PostId:      event.PostId.Bytes(),
 		}, nil
 	case "NewGoodVibes":
-		event := new(XoNewGoodVibes)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoNewGoodVibes
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &NewGoodVibes{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			PostId:      event.PostId.Bytes(),
 		}, nil
 	case "Post":
-		event := new(XoPost)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoPost
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Post{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			Poster:      event.Poster.Bytes(),
 			PostId:      event.PostId.Bytes(),
 		}, nil
 	case "RoleGranted":
-		event := new(XoRoleGranted)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoRoleGranted
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &RoleGranted{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			Role:        event.Role[:],
 			Account:     event.Account.Bytes(),
 			Sender:      event.Sender.Bytes(),
 		}, nil
 	case "Initialized":
-		event := new(XoInitialized)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoInitialized
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Initialized{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			Version:     event.Version,
 		}, nil
 	case "NewMutualLike":
-		event := new(XoNewMutualLike)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoNewMutualLike
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &NewMutualLike{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			MyId:        event.MyId.Bytes(),
 			TargetId:    event.TargetId.Bytes(),
 		}, nil
 	case "RoleRevoked":
-		event := new(XoRoleRevoked)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoRoleRevoked
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &RoleRevoked{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			Role:        event.Role[:],
 			Account:     event.Account.Bytes(),
 			Sender:      event.Sender.Bytes(),
 		}, nil
 	case "Swiped":
-		event := new(XoSwiped)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoSwiped
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Swiped{
 			Ts:           ts,
 			BlockNumber:  vLog.BlockNumber,
 			TxHash:       vLog.TxHash.Bytes(),
-			Index:        uint64(vLog.Index),
+			LogIndex:     uint64(vLog.Index),
 			Sender:       event.Sender.Bytes(),
 			SwipedUserId: event.SwipedUserId.Bytes(),
 			CardId:       event.CardId.Bytes(),
 		}, nil
 	case "Streak":
-		event := new(XoStreak)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoStreak
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &Streak{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			UserId:      event.UserId.Bytes(),
 			Streak:      event.Streak.String(),
 		}, nil
 	case "SaveStreak":
-		event := new(XoSaveStreak)
-		if err := common.UnpackLog(c.Abi, event, ev.Name, vLog); err != nil {
+		var event XoSaveStreak
+		if err := common.UnpackLog(c.Abi, &event, ev.Name, vLog); err != nil {
 			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
 		}
 		return &SaveStreak{
 			Ts:          ts,
 			BlockNumber: vLog.BlockNumber,
 			TxHash:      vLog.TxHash.Bytes(),
-			Index:       uint64(vLog.Index),
+			LogIndex:    uint64(vLog.Index),
 			UserId:      event.UserId.Bytes(),
 			Streak:      event.Streak.String(),
 		}, nil
