648be4f8786e6c0cfa02083752bab1504476ddf0
Sr. D <h@trylotus.xyz>
2024-10-31 16:56:54 +0700

    feat(uniswapv3): update connector lib v1.3.9

--- uniswapv3/factory/factory.go
+++ uniswapv3/factory/factory.go
@@ -16,32 +16,32 @@ func init() {
 	var err error
 	factoryAbi, err = abi.JSON(strings.NewReader(FactoryMetaData.ABI))
 	if err != nil {
-		log.Fatal().Err(err).Msg("error reading factory abi")
+		log.Fatal().Err(err).Msg("Failed to read factory ABI")
 	}
 }
 
 func UnpackLog(vLog types.Log) (interface{}, error) {
 	ev, err := factoryAbi.EventByID(vLog.Topics[0])
 	if err != nil {
-		return nil, fmt.Errorf("cannot find event by ID: %v", err)
+		return nil, fmt.Errorf("no event with id: %s", vLog.Topics[0].Hex())
 	}
 	switch ev.Name {
 	case "FeeAmountEnabled":
 		var event FactoryFeeAmountEnabled
 		if err := common.UnpackLog(factoryAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "OwnerChanged":
 		var event FactoryOwnerChanged
 		if err := common.UnpackLog(factoryAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "PoolCreated":
 		var event FactoryPoolCreated
 		if err := common.UnpackLog(factoryAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	default:
--- uniswapv3/pool/pool.go
+++ uniswapv3/pool/pool.go
@@ -16,68 +16,68 @@ func init() {
 	var err error
 	poolAbi, err = abi.JSON(strings.NewReader(PoolMetaData.ABI))
 	if err != nil {
-		log.Fatal().Err(err).Msg("error reading pool abi")
+		log.Fatal().Err(err).Msg("Failed to read pool ABI")
 	}
 }
 
 func UnpackLog(vLog types.Log) (interface{}, error) {
 	ev, err := poolAbi.EventByID(vLog.Topics[0])
 	if err != nil {
-		return nil, fmt.Errorf("cannot find event by ID: %v", err)
+		return nil, fmt.Errorf("no event with id: %s", vLog.Topics[0].Hex())
 	}
 	switch ev.Name {
 	case "Burn":
 		var event PoolBurn
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "Collect":
 		var event PoolCollect
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "CollectProtocol":
 		var event PoolCollectProtocol
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "Flash":
 		var event PoolFlash
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "IncreaseObservationCardinalityNext":
 		var event PoolIncreaseObservationCardinalityNext
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "Initialize":
 		var event PoolInitialize
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "Mint":
 		var event PoolMint
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "SetFeeProtocol":
 		var event PoolSetFeeProtocol
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	case "Swap":
 		var event PoolSwap
 		if err := common.UnpackLog(poolAbi, &event, ev.Name, vLog); err != nil {
-			return nil, fmt.Errorf("error unpacking event: %s", ev.Name)
+			return nil, fmt.Errorf("error unpacking event %s: %w", ev.Name, err)
 		}
 		return event, nil
 	default:
