97a99a8cc7a93dab4080efe02ae531be79ee5506
Sr. D <h@trylotus.xyz>
2024-11-05 15:21:16 +0700

    feat(uniswapv3): update connector lib v1.4.1

--- uniswapv3/cmd/uniswapv3/main.go
+++ uniswapv3/cmd/uniswapv3/main.go
@@ -37,13 +37,9 @@ func main() {
 	ctx, cancel := common.ContextWithSignal(context.Background(), os.Interrupt)
 	defer cancel()
 
-	rpcUrl := os.Getenv("RPC_URL")
-
-	log.Info().Str("url", rpcUrl).Msg("Connecting to RPC")
-
-	client, err := evm.DialContext(ctx, os.Getenv("RPC_URL"))
+	client, err := evm.Connect(ctx, os.Getenv("RPC_URL"), os.Getenv("CACHE_URL"))
 	if err != nil {
-		log.Fatal().Err(err).Str("url", os.Getenv("RPC_URL")).Msg("Failed to connect to RPC")
+		log.Fatal().Err(err).Msg("Failed to create RPC client")
 	}
 
 	store, err := uniswapv3.NewStore(ctx, os.Getenv("DATA_SOURCE"))
