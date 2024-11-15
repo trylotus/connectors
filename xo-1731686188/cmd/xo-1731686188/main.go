65a7e19fc6c1b344e117c3e2d1df8b77cdb5732c
TrylotusBot <TrylotusBot@trylotus.xyz>
2024-11-15 22:56:31 +0700

    [connector-bot] generated connector for Cyber Mainnet|xo-1731686188

---
+++ xo-1731686188/cmd/xo-1731686188/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/joho/godotenv"
+	"github.com/spf13/pflag"
+	"github.com/trylotus/go-connector"
+	"github.com/trylotus/go-connector/common"
+	"github.com/trylotus/go-connector/log"
+	"github.com/trylotus/go-connector/manager"
+	"github.com/trylotus/go-connector/registry"
+	"github.com/trylotus/go-connector/source/evm"
+
+    
+	"github.com/trylotus/connectors/xo-1731686188/contracts/usercontract0"
+)
+
+var ContractList = []evm.SmartContract{
+    
+        usercontract0.NewContract(ethcommon.HexToAddress("0x84583e7d2d92d87d5b3bac850ab4bad37ae568e8")),
+}
+
+func main() {
+	_ = godotenv.Load()
+
+	var (
+		backfill  int64
+		subscribe bool
+	)
+
+	pflag.Int64VarP(&backfill, "backfill", "b", 0, "block number to backfill to")
+	pflag.BoolVarP(&subscribe, "subscribe", "s", true, "whether to subscribe to new blockchain events")
+
+	pflag.Parse()
+
+	ctx, cancel := common.ContextWithSignal(context.Background(), os.Interrupt)
+	defer cancel()
+
+	client, err := evm.Connect(ctx, os.Getenv("RPC_URL"), os.Getenv("CACHE_URL"))
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create RPC client")
+	}
+
+	c := connector.NewConnector(
+		evm.NewSource(client, ContractList...),
+		manager.NewManager(os.Getenv("MANAGER_URL")),
+		registry.NewRegistry(os.Getenv("REGISTRY_URL")),
+		connector.WithDefaultOptions(),
+	)
+
+	go c.RegisterDescriptor(ctx,
+        
+            usercontract0.File_usercontract0_contract_proto,
+	)
+
+	if subscribe {
+		c.Subscribe(ctx)
+	}
+
+	if backfill > 0 {
+		c.Backfill(ctx, backfill)
+	}
+
+	if err := c.Run(ctx); err != nil {
+		log.Error().Err(err).Msg("Connector shutdown")
+		return
+	}
+
+	log.Info().Msg("Connector shutdown")
+}
