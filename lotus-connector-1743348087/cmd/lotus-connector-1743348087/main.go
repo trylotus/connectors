772e382b920929ca9413b030f3c4af09ec39c39c
TrylotusBot <TrylotusBot@trylotus.xyz>
2025-03-30 23:21:30 +0800

    [connector-bot] generated connector for Ethereum Mainnet|lotus-connector-1743348087

---
+++ lotus-connector-1743348087/cmd/lotus-connector-1743348087/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"os"
+
+	ethcommon "github.com/ethereum/go-ethereum/common"
+	"github.com/joho/godotenv"
+	"github.com/spf13/pflag"
+
+	"github.com/starbloom-ai/go-connector"
+	"github.com/starbloom-ai/go-connector/common"
+	"github.com/starbloom-ai/go-connector/kafka"
+	"github.com/starbloom-ai/go-connector/log"
+	"github.com/starbloom-ai/go-connector/manager"
+	"github.com/starbloom-ai/go-connector/registry"
+	"github.com/starbloom-ai/go-connector/source/evm"
+
+    
+)
+
+var ContractList = []evm.SmartContract{
+    
+}
+
+func main() {
+	_ = godotenv.Load()
+
+	var (
+		backfill  int64
+		subscribe bool
+	)
+
+	pflag.Int64VarP(&backfill, "backfill", "b", 0, "block number to backfill to")
+	pflag.BoolVarP(&subscribe, "subscribe", "s", true, "whether to subscribe to new blockchain events")
+
+	pflag.Parse()
+
+	ctx, cancel := common.ContextWithSignal(context.Background(), os.Interrupt)
+	defer cancel()
+
+	client, err := evm.Connect(ctx, os.Getenv("RPC_URL"), os.Getenv("CACHE_URL"))
+	if err != nil {
+		log.Fatal().Err(err).Msg("Failed to create RPC client")
+	}
+
+	c := connector.NewConnector(
+		evm.NewSource(client, ContractList...),
+		manager.NewManager(os.Getenv("MANAGER_URL")),
+		registry.NewRegistry(os.Getenv("REGISTRY_URL")),
+		kafka.NewProducerFromFile(common.EnvString("KAFKA_CONFIG", "kafka.json")),
+		connector.WithDefaultOptions(),
+	)
+
+	go c.RegisterDescriptor(ctx,
+        
+	)
+
+	c.Run(ctx, backfill, subscribe)
+}
